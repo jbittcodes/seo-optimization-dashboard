fe493538bd7274d4dc91ab84b78e24227f54b96a
jbittcodes <jamesbitting76@gmail.com>
2024-09-18 09:48:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>SEO Project</h1>
+        <p>This project demonstrates SEO best practices.</p>
+    </div>
+);
+
+export default App;
